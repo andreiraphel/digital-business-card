a49e41929b7071888e9822bf89fd557e4306d1ea
andreiraphel <andreiraphel@gmail.com>
2025-07-21 08:28:52 +0800

    moved styles.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <small>Copyright 2025 Andrei Raphel Amang. All right reserve</small>
+            <small>Copyright © 2025 Andrei Raphel Amang. All right reserve</small>
         </footer>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import myLogo from "../assets/aa-nobg.png";
 export default function Navbar() {
     return (
         <>
-            <img src={myLogo} alt="logo" className="my-logo" />
+            <a href="/" ><img src={myLogo} alt="logo" className="my-logo" /></a>
             <ul className="nav-list">
                 <li className="nav-option"><a href="#">ABOUT ME</a></li>
                 <li className="nav-option"><a href="#">MY WORK</a></li>
